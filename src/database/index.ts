b44ce110bb948e81784655e5cf87d5624cae4084
Mateus Fugita <mateushiwao@gmail.com>
2021-12-07 20:22:06 -0300

    feat: read ormconfig from .env file

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,12 +2,5 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
-    
-    return createConnection(
-        Object.assign(defaultOptions, {
-            database: process.env.NODE_ENV === 'test'
-             ? './src/database/database.test.sqlite'
-             : defaultOptions.database
-        })
-    );
+    return createConnection(defaultOptions);
 }
